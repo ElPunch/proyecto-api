c61babb075b86aa8fc9e73373fdcdf960d801f13
ElPunch <alfonsorobledo3548@gmail.com>
2025-06-05 15:40:20 -0600

    inicio proyecto

---
+++ Archivos Js/ScriptFetch.js
@@ -0,0 +1,6 @@
+fetch ("https://superheroapi.com/api/5cbbe2217a57e86e8968274677c361c0/245")
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+.catch(error => alert (error))
