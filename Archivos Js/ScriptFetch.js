50ee8ba96bba991e99bb824ddc28b3edf0e8d487
ElPunch <alfonsorobledo3548@gmail.com>
2025-06-07 17:13:04 -0600

    creacion del index

--- Archivos Js/ScriptFetch.js
+++ Archivos Js/ScriptFetch.js
@@ -1,6 +1,27 @@
-fetch ("https://superheroapi.com/api.php/5cbbe2217a57e86e8968274677c361c0/1")
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
-.catch(error => alert (error))
+fetchData();
+
+async function fetchData(){
+    try{
+
+        const NombreHeroe = document.getElementById("NombreHeroe").value.toLowerCase();
+        
+        const response = await fetch (`https://superheroapi.com/api.php/5cbbe2217a57e86e8968274677c361c0/search/${NombreHeroe}`);
+
+        if(!response.ok){
+            throw new Error("No se pudo consumir el recurso");
+        }
+
+        const data = await response.json();
+       
+        const imagenHeroe = data.results[0].image.url;
+        const imagen = document.getElementById("ImagenHeroe");
+
+        imagen.src = imagenHeroe;
+        imagen.style.display = "block";
+        console.log(data);
+    
+    }
+    catch(error){
+        console.error(error);
+    }
+}
